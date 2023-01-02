0fd855106da9024cd0c49540abf6d064f7b7be94
lollcat <laurencemidgley@gmail.com>
2023-01-02 12:58:29 +0000

    feat: remove realnvp that didn't work

--- flow/distribution.py
+++ flow/distribution.py
@@ -1,10 +1,7 @@
 import distrax
-import haiku as hk
-import jax.random
 
 from base import CentreGravityGaussian
 from bijector_proj_real_nvp import make_se_equivariant_split_coupling_with_projection
-from bijector_simple_real_nvp import make_se_equivariant_split_coupling_simple
 from bijector_nice import make_se_equivariant_nice
 
 
@@ -20,7 +17,7 @@ def make_equivariant_augmented_flow_dist(dim, nodes, n_layers, type="proj", flow
         elif type == "nice":
             bijector = make_se_equivariant_nice(dim, swap=swap, identity_init=flow_identity_init)
         else:
-            bijector = make_se_equivariant_split_coupling_simple(dim, swap=swap)
+            raise NotImplemented
         bijectors.append(bijector)
 
     flow = distrax.Chain(bijectors)
