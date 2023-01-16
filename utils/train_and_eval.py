8e1ca24fa5604221d645896912f18d4c27ce2a55
lollcat <laurencemidgley@gmail.com>
2023-01-16 15:00:24 +0000

    feat: effective sample size

--- utils/train_and_eval.py
+++ utils/train_and_eval.py
@@ -1,3 +1,4 @@
+import chex
 import numpy as np
 import jax
 import jax.numpy as jnp
@@ -29,12 +30,16 @@ def get_target_augmented_variables(x_original, key):
     jax.random.normal(key, shape=x_original.shape)
     return x_augmented
 
-
-def get_marginal_log_lik(log_prob_fn, x_original, key, K: int = 10):
+def get_augmented_dist(x_original):
+    chex.assert_rank(x_original, 3)
     augmented_mean = jnp.mean(x_original, axis=(1, 2), keepdims=True)
     augmented_mean = jnp.broadcast_to(augmented_mean, x_original.shape)
     augmented_dist = distrax.Independent(distrax.MultivariateNormalDiag(loc=augmented_mean),
                                          reinterpreted_batch_ndims=1)
+    return augmented_dist
+
+def get_marginal_log_lik(log_prob_fn, x_original, key, K: int = 10):
+    augmented_dist = get_augmented_dist(x_original)
     augmented_var, log_p_a = augmented_dist._sample_n_and_log_prob(n=K, key=key)
     x_original = jnp.stack([x_original]*K, axis=0)
     log_q = jax.vmap(log_prob_fn)(jnp.concatenate((x_original, augmented_var), axis=-1))
@@ -52,7 +57,9 @@ def eval_fn(params, x, key, flow_log_prob_fn, flow_sample_and_log_prob_fn, targe
 
     # Calculate ESS
     x_flow, log_prob_flow = flow_sample_and_log_prob_fn.apply(params, key2, (x.shape[0]))
-    log_w = target_log_prob(x_flow) - log_prob_flow
+    x_flow_original, x_flow_aug = jnp.split(x_flow, axis=-1, indices_or_sections=2)
+    augmented_dist = get_augmented_dist(x_flow_original)
+    log_w = target_log_prob(x_flow_original) + augmented_dist.log_prob(x_flow_aug) - log_prob_flow
     ess = 1 / jnp.sum(jax.nn.softmax(log_w) ** 2) / log_w.shape[0]
 
     info = {"eval_marginal_log_lik": marginal_log_lik,
