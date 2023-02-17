12aeff6972bab0c996c5b7af3be7060a1c0d57d5
lollcat <laurencemidgley@gmail.com>
2023-02-17 16:26:16 +0000

    feat: scale_along_vector tests passing

--- flow/bijectors/bijector_scale_along_vector.py
+++ flow/bijectors/bijector_scale_along_vector.py
@@ -2,7 +2,7 @@ import distrax
 import jax.nn
 import jax.numpy as jnp
 
-from nets.en_gnn import se_equivariant_net, EgnnConfig
+from nets.base import NetsConfig, build_egnn_fn
 
 
 def make_conditioner(ref_and_scale_equivariant_fn, activation_fn):
@@ -18,21 +18,20 @@ def make_conditioner(ref_and_scale_equivariant_fn, activation_fn):
     return conditioner
 
 
-def make_se_equivariant_scale_along_vector(layer_number, dim, swap, egnn_config: EgnnConfig, identity_init: bool = True,
+def make_se_equivariant_scale_along_vector(layer_number, dim, swap, nets_config: NetsConfig, identity_init: bool = True,
                                            activation_fn = jax.nn.softplus):
     """Flow is x + (x - r)*scale where scale is an invariant scalar, and r is equivariant reference point"""
 
-    ref_and_scale_equivariant_fn = se_equivariant_net(
-        egnn_config._replace(name=f"layer_{layer_number}_swap{swap}_ref",
-                           identity_init_x=False,
-                           zero_init_h=identity_init,
-                           h_config=egnn_config.h_config._replace(h_out_dim=1, h_out=True)))
+    equivariant_fn = build_egnn_fn(name=f"layer_{layer_number}_swap{swap}",
+                                   nets_config=nets_config,
+                                   n_equivariant_vectors_out=1,
+                                   n_invariant_feat_out=1)
 
     def bijector_fn(params):
         scale, shift = params
         return distrax.ScalarAffine(scale=scale, shift=shift)
 
-    conditioner = make_conditioner(ref_and_scale_equivariant_fn, activation_fn=activation_fn)
+    conditioner = make_conditioner(equivariant_fn, activation_fn=activation_fn)
     return distrax.SplitCoupling(
         split_index=dim,
         event_ndims=2,  # [nodes, dim]
