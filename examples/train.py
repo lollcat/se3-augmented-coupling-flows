475cc533a38e3bf92ec93fe498414e139a402eb6
lollcat <laurencemidgley@gmail.com>
2023-03-19 12:41:02 +0000

    feat: make lj13 config similar to qm9

--- examples/train.py
+++ examples/train.py
@@ -143,19 +143,19 @@ class OptimizerConfig(NamedTuple):
 def get_optimizer_and_step_fn(optimizer_config: OptimizerConfig, n_iter_per_epoch, total_n_epoch):
     if optimizer_config.use_schedule:
         lr = optax.warmup_cosine_decay_schedule(
-            init_value=optimizer_config.init_lr,
-            peak_value=optimizer_config.peak_lr,
-            end_value=optimizer_config.end_lr,
+            init_value=float(optimizer_config.init_lr),
+            peak_value=float(optimizer_config.peak_lr),
+            end_value=float(optimizer_config.end_lr),
             warmup_steps=optimizer_config.warmup_n_epoch * n_iter_per_epoch,
             decay_steps=n_iter_per_epoch*total_n_epoch
                                                      )
     else:
-        lr = optimizer_config.init_lr
+        lr = float(optimizer_config.init_lr)
 
     grad_transforms = [optax.zero_nans()]
 
     if optimizer_config.max_global_norm:
-        clipping_fn = optax.clip_by_global_norm(optimizer_config.max_global_norm)
+        clipping_fn = optax.clip_by_global_norm(float(optimizer_config.max_global_norm))
         grad_transforms.append(clipping_fn)
     else:
         pass
