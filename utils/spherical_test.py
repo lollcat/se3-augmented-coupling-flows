ebd1e984cb9a33b0224ac8a4019346ed8ba9ab8b
lollcat <laurencemidgley@gmail.com>
2023-04-04 12:13:45 +0100

    feat: add spherical tests back

--- utils/spherical_test.py
+++ utils/spherical_test.py
@@ -40,37 +40,40 @@ def tesst_polar_does_not_smoke_and_invertible():
     chex.assert_trees_all_close(log_det_fwd, -log_det_rv, rtol=1e-5)
 
 
-# def tesst_to_spherical_is_invariant():
-#     key = jax.random.PRNGKey(0)
-#
-#     def invariant_fn(x):
-#         chex.assert_shape(x, (4, 1, 3))  # Need multiplicity axis for `test_fn_is_invariant`.
-#         x = jnp.squeeze(x, axis=1)
-#         x, a, b, c = jnp.split(x, 4, axis=0)
-#         x, a, b, c = jnp.squeeze(x, axis=0), jnp.squeeze(a, axis=0), jnp.squeeze(b, axis=0), jnp.squeeze(c, axis=0)
-#         return to_spherical_and_log_det(x, a, b, c)[0]
-#
-#
-#     event_shape = (4, 1, 3)
-#     test_fn_is_invariant(invariant_fn = invariant_fn, key=key, event_shape=event_shape, translate=True)
-#
-#
-# def tesst_to_spherical_and_back_is_equivariant():
-#     key = jax.random.PRNGKey(0)
-#
-#     def equivariant_fn(x):
-#         chex.assert_shape(x, (4, 1, 3))  # Need multiplicity axis for `test_fn_is_invariant`.
-#         x = jnp.squeeze(x, axis=1)
-#         x, a, b, c = jnp.split(x, 4, axis=0)
-#         x, a, b, c = jnp.squeeze(x, axis=0), jnp.squeeze(a, axis=0), jnp.squeeze(b, axis=0), jnp.squeeze(c, axis=0)
-#         sph_x = to_spherical_and_log_det(x, a, b, c)
-#         sph_x_new = sph_x + 0.01
-#         x_new = to_cartesian(sph_x_new, a, b, c)
-#         return x_new[None, None, :]  # Need to have same rank as input.
-#
-#
-#     event_shape = (4, 1, 3)
-#     test_fn_is_equivariant(equivariant_fn=equivariant_fn, key=key, event_shape=event_shape, translate=True)
+def tesst_to_spherical_is_invariant():
+    # Currently for 3D.
+    key = jax.random.PRNGKey(0)
+
+    def invariant_fn(x_and_ref):
+        # Note: Invariant to rotations to both x and reference, hence package them as single input for this test.
+        chex.assert_shape(x_and_ref, (4, 1, 3))  # Need multiplicity axis for `test_fn_is_invariant`.
+        x_and_ref = jnp.squeeze(x_and_ref, axis=1)
+        x, reference = jnp.split(x_and_ref, [1,], axis=0)
+        x = jnp.squeeze(x, axis=0)
+        return to_spherical_and_log_det(x, reference)[0]
+
+    event_shape = (4, 1, 3)
+    test_fn_is_invariant(invariant_fn = invariant_fn, key=key, event_shape=event_shape, translate=True)
+
+
+def tesst_to_spherical_and_back_is_equivariant():
+    # Currently for 3D.
+    key = jax.random.PRNGKey(0)
+
+    def equivariant_fn(x_and_ref):
+        # Note: Invariant to rotations to both x and reference, hence package them as single input for this test.
+        chex.assert_shape(x_and_ref, (4, 1, 3))  # Need multiplicity axis for `test_fn_is_invariant`.
+        x_and_ref = jnp.squeeze(x_and_ref, axis=1)
+        x, reference = jnp.split(x_and_ref, [1,], axis=0)
+        x = jnp.squeeze(x, axis=0)
+        sph_x = to_spherical_and_log_det(x, reference)[0]
+        sph_x_new = sph_x + 0.01
+        x_new = to_cartesian_and_log_det(sph_x_new, reference)[0]
+        return x_new[None, None, :]  # Need to have same rank as input.
+
+
+    event_shape = (4, 1, 3)
+    test_fn_is_equivariant(equivariant_fn=equivariant_fn, key=key, event_shape=event_shape, translate=True)
 
 
 
@@ -82,6 +85,6 @@ if __name__ == '__main__':
 
     tesst_polar_does_not_smoke_and_invertible()
     tesst_does_not_smoke_and_invertible()
-    # tesst_to_spherical_is_invariant()
-    # tesst_to_spherical_and_back_is_equivariant()
+    tesst_to_spherical_is_invariant()
+    tesst_to_spherical_and_back_is_equivariant()
     print("All tests passed")
