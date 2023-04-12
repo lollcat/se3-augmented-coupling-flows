405bf813f6a98d27cc96a372a80cc60a3dba7200
Vincent Stimper <vincent.stimper@gmail.com>
2023-04-12 19:06:02 +0200

    Added base distribution from internal coordinates for aldp

--- flow/base_dist.py
+++ flow/base_dist.py
@@ -1,5 +1,5 @@
 """Following https://github.com/vgsatorras/en_flows/blob/main/flows/utils.py."""
-from typing import Tuple, Optional
+from typing import Tuple, List, Optional
 
 import chex
 import jax
@@ -8,6 +8,9 @@ import numpy as np
 from chex import PRNGKey, Array
 import distrax
 import haiku as hk
+import torch
+from boltzgen.flows import CoordinateTransform
+import mdtraj
 
 from flow.conditional_dist import get_conditional_gaussian_augmented_dist
 from flow.distrax_with_extra import DistributionWithExtra, Extra
@@ -86,7 +89,7 @@ class CentreGravitryGaussianAndCondtionalGuassian(DistributionWithExtra):
 
 
 class CentreGravityGaussian(distrax.Distribution):
-    """Guassian distribution over nodes in space, with a zero centre of gravity.
+    """Gaussian distribution over nodes in space, with a zero centre of gravity.
     See https://arxiv.org/pdf/2105.09016.pdf."""
     def __init__(self, dim: int, n_nodes: int, log_scale: chex.Array = jnp.zeros(())):
 
@@ -113,6 +116,180 @@ class CentreGravityGaussian(distrax.Distribution):
     def event_shape(self) -> Tuple[int, ...]:
         return (self.n_nodes, self.dim)
 
+
+class AldpTransformedInternalsAndConditionalGaussian(distrax.Distribution):
+    """x ~ AldpTransformedInternals, a ~ Normal(x, I)"""
+    def __init__(self, data_path: str):
+        self.aldp = AldpTransformedInternals(data_path)
+
+
+    def __init__(self, data_path: str, n_aux: int,
+                 global_centering: bool = False,
+                 augmented_scale_init: float = 1.0,
+                 trainable_augmented_scale: bool = True):
+        self.n_aux = n_aux
+        self.dim = 3
+        self.n_nodes = 22
+        self.global_centering = global_centering
+        if trainable_augmented_scale:
+            self.augmented_log_scale = hk.get_parameter(name='augmented_log_scale', shape=(n_aux,),
+                                                          init=hk.initializers.Constant(jnp.log(augmented_scale_init)))
+        else:
+            self.augmented_log_scale = jnp.zeros((n_aux,))
+        self.x_dist = AldpTransformedInternals(data_path)
+
+
+    def get_augmented_dist(self, x: chex.Array) -> distrax.Distribution:
+        dist = get_conditional_gaussian_augmented_dist(
+            x, n_aux=self.n_aux, scale=jnp.exp(self.augmented_log_scale), global_centering=self.global_centering)
+        return dist
+
+
+    def _sample_n(self, key: PRNGKey, n: int) -> Array:
+        key1, key2 = jax.random.split(key)
+        original_coords = self.x_dist._sample_n(key1, n)
+        augmented_coords = self.get_augmented_dist(original_coords).sample(seed=key2)
+        joint_coords = jnp.concatenate([jnp.expand_dims(original_coords, axis=-2), augmented_coords], axis=-2)
+        return joint_coords
+
+    def log_prob(self, value: Array) -> Array:
+        original_coords, augmented_coords = jnp.split(value, indices_or_sections=[1, ], axis=-2)
+        assert original_coords.shape[-2] == 1
+        assert augmented_coords.shape[-2] == self.n_aux
+        original_coords = jnp.squeeze(original_coords, axis=-2)
+
+        log_prob_a_given_x = self.get_augmented_dist(original_coords).log_prob(augmented_coords)
+        log_p_x = self.x_dist.log_prob(original_coords)
+        chex.assert_equal_shape((log_p_x, log_prob_a_given_x))
+        return log_p_x + log_prob_a_given_x
+
+    def get_extra(self) -> Extra:
+        extra = Extra(aux_info={},
+                      info_aggregator={})
+        for i in range(self.n_aux):
+            extra.aux_info[f"base_a{i}_scale"] = jnp.exp(self.augmented_log_scale[i])
+            extra.info_aggregator[f"base_a{i}_scale"] = jnp.mean
+        return extra
+
+    def log_prob_with_extra(self, value: Array) -> Tuple[Array, Extra]:
+        extra = self.get_extra()
+        log_prob = self.log_prob(value)
+        return log_prob, extra
+
+    def sample_n_and_log_prob_with_extra(self, key: PRNGKey, n: int) -> Tuple[Array, Array, Extra]:
+        x, log_prob = self._sample_n_and_log_prob(key, n)
+        extra = self.get_extra()
+        return x, log_prob, extra
+
+    @property
+    def event_shape(self) -> Tuple[int, ...]:
+        return (self.n_nodes, self.n_aux+1, self.dim)
+
+
+class AldpTransformedInternals(distrax.Distribution):
+    def __init(self, data_path: str):
+        traj = mdtraj.load(data_path)
+
+        ndim = 66
+        transform_data = torch.tensor(np.array(traj.xyz).reshape(-1, ndim),
+                                      dtype=torch.float64)
+        z_matrix = [
+            (0, [1, 4, 6]),
+            (1, [4, 6, 8]),
+            (2, [1, 4, 0]),
+            (3, [1, 4, 0]),
+            (4, [6, 8, 14]),
+            (5, [4, 6, 8]),
+            (7, [6, 8, 4]),
+            (9, [8, 6, 4]),
+            (10, [8, 6, 4]),
+            (11, [10, 8, 6]),
+            (12, [10, 8, 11]),
+            (13, [10, 8, 11]),
+            (15, [14, 8, 16]),
+            (16, [14, 8, 6]),
+            (17, [16, 14, 15]),
+            (18, [16, 14, 8]),
+            (19, [18, 16, 14]),
+            (20, [18, 16, 19]),
+            (21, [18, 16, 19])
+        ]
+        cart_indices = [8, 6, 14]
+        ind_circ_dih = [0, 1, 2, 3, 4, 5, 8, 9, 10, 13, 15, 16]
+        self.transform = CoordinateTransform(transform_data, ndim, z_matrix, cart_indices,
+                                             mode="internal", ind_circ_dih=ind_circ_dih)
+
+        ncarts = self.transform.transform.len_cart_inds
+        permute_inv = self.transform.transform.permute_inv.cpu().numpy()
+        dih_ind_ = self.transform.transform.ic_transform.dih_indices.cpu().numpy()
+        std_dih = self.transform.transform.ic_transform.std_dih.cpu().numpy()
+
+        ind = np.arange(60)
+        ind = np.concatenate([ind[:3 * ncarts - 6], -np.ones(6, dtype=np.int), ind[3 * ncarts - 6:]])
+        ind = ind[permute_inv]
+        dih_ind = ind[dih_ind_]
+
+        ind_circ = dih_ind[ind_circ_dih]
+        bound_circ = np.pi / std_dih[ind_circ_dih]
+
+        scale = jnp.ones(60)
+        scale[ind_circ] = bound_circ
+        self.dist_internal = UniformGaussian(dim=ndim, ind_uniform=ind_circ, scale=scale)
+
+    def _sample_n(self, key: PRNGKey, n: int) -> Array:
+        z = self.dist_internal.sample(key, n)
+        z_torch = torch.tensor(np.array(z).reshape(-1, 60),
+                               dtype=torch.float64)
+        x_np = self.transform(z_torch)[0].detach().numpy()
+        x = jnp.array(x_np.reshape(-1, 22, 3))
+        return remove_mean(x)
+
+    def log_prob(self, value: Array) -> Array:
+        x_torch = torch.tensor(np.array(value).reshape(-1, 66),
+                               dtype=torch.float64)
+        z_torch, log_det_torch = self.transform.inverse(x_torch)
+        z_np, log_det_np = z_torch.detach().numpy(), log_det_torch.detach().numpy()
+        return self.dist_internal.log_prob(jnp.array(z_np)) + jnp.array(log_det_np)
+
+    @property
+    def event_shape(self) -> Tuple[int]:
+        return (22, 3)
+
+
+
+class UniformGaussian(distrax.Distribution):
+    def __init__(self, dim: int, ind_uniform: List[int], scale: Optional[chex.Array] = None):
+        self.dim = dim
+        self.ind_uniform = jnp.array(ind_uniform)
+        self.ind_gaussian = jnp.array([i for i in range(dim) if i not in ind_uniform])
+        perm = jnp.concatenate([self.ind_uniform, self.ind_gaussian])
+        inv_perm = jnp.zeros_like(perm)
+        for i in range(dim):
+            inv_perm[perm[i]] = i
+        self.inv_perm = inv_perm
+        if scale is None:
+            self.scale = jnp.ones((dim,))
+        else:
+            self.scale = scale
+
+    def _sample_n(self, key: PRNGKey, n: int) -> Array:
+        key_u, key_g = jax.random.split(key)
+        eps_u = jax.random.uniform(key_u, shape=(n, self.ind_uniform.shape[0]), minval=-1, maxval=1)
+        eps_g = jax.random.normal(key_g, shape=(n, self.ind_gaussian.shape[0]))
+        z = jnp.concatenate([eps_u, eps_g], axis=-1)
+        z = z[:, self.inv_perm]
+        return z * self.scale
+
+    def log_prob(self, value: Array) -> Array:
+        log_prob_u = jnp.broadcast_to(-jnp.log(2 * self.scale[self.ind_uniform]), (value.shape[0], -1))
+        log_prob_g = - 0.5 * np.log(2 * np.pi) \
+                     - jnp.log(self.scale[self.ind_gaussian]) \
+                     - 0.5 * (value[:, self.ind_gaussian] / self.scale[self.ind_gaussian]) ** 2
+        return jnp.sum(log_prob_u, -1) + jnp.sum(log_prob_g, -1)
+
+    @property
+    def event_shape(self) -> Tuple[int, ...]:
+        return (self.dim,)
 
 
 
