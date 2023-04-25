c4ca70dd024dd8d714b095096bf487900f13b48c
Vincent Stimper <vincent.stimper@gmail.com>
2023-04-25 19:50:27 +0200

    feat: internal coordinates base distribution (#53)
    * Added base distribution from internal coordinates for aldp
    
    * Added internal coordinates base distribution
    
    * Fixed typing
    
    * Fixed arguments for base distribution
    
    * Added arguments for base distribution
    
    * Fixed constructor name
    
    * Fixed setting scale
    
    * Fixed inverse permutation setup
    
    * Fixed broadcasting
    
    * Base distribution can handle data without batch dim now
    
    * Fixed indexing of value
    
    * adapted aldp x base to jax transform
    
    * Added internal coordinates base distribution for x
    
    * Internal coordinate dist added to base
    
    * Do not convert to np to allow jittability
    
    * Made jittable
    
    * Tried to make index generation jittable
    
    * Trying different method to avoid concretization error
    
    * Another attempt to make things jittable

--- flow/build_flow.py
+++ flow/build_flow.py
@@ -6,7 +6,7 @@ from typing import NamedTuple, Sequence, Union, Iterable
 import distrax
 
 from flow.base_dist import JointBaseDistribution
-from flow.x_base_dist import CentreGravityGaussian, HarmonicPotential
+from flow.x_base_dist import CentreGravityGaussian, HarmonicPotential, AldpTransformedInternals
 from flow.conditional_dist import build_aux_target_dist
 from flow.bijectors.build_proj_coupling import make_proj_coupling_layer
 from flow.bijectors.equi_nice import make_se_equivariant_nice
@@ -27,10 +27,11 @@ class ConditionalAuxDistConfig(NamedTuple):
     scale_init: float = 1.0
 
 class XDistConfig(NamedTuple):
-    type: str = 'centre_gravity_gaussian'  # centre_gravity_gaussian or harmonic_potential
+    type: str = 'centre_gravity_gaussian'  # centre_gravity_gaussian, harmonic_potential, or aldp_internal_coordinates
     a: float = 1.0
     edges: Iterable = []
     trainable_mode_scale: bool = False
+    data_path: str = ''
 
 class BaseConfig(NamedTuple):
     x_dist: XDistConfig = XDistConfig()
@@ -67,13 +68,16 @@ def create_flow_recipe(config: FlowDistConfig) -> AugmentedFlowRecipe:
             assert config.act_norm is False
 
     def make_base() -> distrax.Distribution:
-        assert config.base.x_dist.type in ['centre_gravity_gaussian', 'harmonic_potential']
+        assert config.base.x_dist.type in ['centre_gravity_gaussian', 'harmonic_potential',
+                                           'aldp_internal_coordinates']
         if config.base.x_dist.type == 'centre_gravity_gaussian':
             x_dist = CentreGravityGaussian(dim=config.dim, n_nodes=config.nodes)
         elif config.base.x_dist.type == 'harmonic_potential':
             x_dist = HarmonicPotential(dim=config.dim, n_nodes=config.nodes, a=config.base.x_dist.a,
                                        edges=config.base.x_dist.edges,
                                        trainable_mode_scale=config.base.x_dist.trainable_mode_scale)
+        elif config.base.x_dist.type == 'aldp_internal_coordinates':
+            x_dist = AldpTransformedInternals(data_path=config.base.x_dist.data_path)
         base = JointBaseDistribution(
             dim=config.dim,
             n_nodes=config.nodes,
