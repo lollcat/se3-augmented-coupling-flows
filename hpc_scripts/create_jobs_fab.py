de3986ddb9cb348e88244e48a831dccd9a8260ae
lollcat <laurencemidgley@gmail.com>
2023-05-04 09:51:19 +0100

    feat: lj13 setup hyper-param search

--- hpc_scripts/create_jobs_fab.py
+++ hpc_scripts/create_jobs_fab.py
@@ -1,22 +1,23 @@
 from pathlib import Path
-from hashlib import md5
 
 ################################### Configs #####################################
 
 EXPERIMENT_NAME = 'fab'  # no underscores.
 JOBS_FOLDER = f'trial-{EXPERIMENT_NAME}'
 DELETE_PREV_FOLDER = True
-SCRIPT_NAMES = ['dw4','lj13'] # , 'lj13', 'qm9']
-FLOW_TYPES=['non_equivariant'] # ['spherical', 'along_vector', 'proj','non_equivariant']
+SCRIPT_NAMES = ['lj13']  #  ['dw4' , 'lj13']
+FLOW_TYPES=['spherical'] # ['spherical', 'along_vector', 'proj','non_equivariant']
 SCRIPT_FOLDER = 'examples/' #  f'home/lim24/rds/hpc-work/lim24/augmented-equivariant-flows/examples/'
 RANDOM_SEEDS = [0,]
-
+N_UPDATES_PER_SMC_FORWARD_PASS = [8, 16, 32]
+LR_s = [4e-4, 2e-4, 1e-4]
+N_LAYERS=[12, 16]
+BATCH_SIZES=[64,128]
 ################################################################################
 
 times = {
     'dw4': '12:00:00',
-    'lj13': '23:00:00',
-    'qm9': '24:00:00',
+    'lj13': '32:00:00',
 }
 
 jobsfolder = Path(f'./{JOBS_FOLDER}')
@@ -35,9 +36,17 @@ for script in SCRIPT_NAMES:
 
     with open(jobsfile, 'w') as f:
         for flow_type in FLOW_TYPES:
-            for random_seed in RANDOM_SEEDS:
-                line = (f'{SCRIPT_FOLDER + script}_fab.py '
-                        f'flow.type={flow_type} '
-                        f'training.seed={random_seed} '
-                        )
-                f.write(line + '\n')
+            for smc_updates_per_smc_forward_pass in N_UPDATES_PER_SMC_FORWARD_PASS:
+                for lr in LR_s:
+                    for n_layers in N_LAYERS:
+                        for batch_size in BATCH_SIZES:
+                            for random_seed in RANDOM_SEEDS:
+                                line = (f'{SCRIPT_FOLDER + script}_fab.py '
+                                        f'flow.type={flow_type} '
+                                        f'flow.n_layers={n_layers} '
+                                        f'fab.smc_updates_per_smc_forward_pass={smc_updates_per_smc_forward_pass} '
+                                        f'training.seed={random_seed} '
+                                        f'training.optimizer.init_lr={lr} '
+                                        f'training.batch_size={batch_size} '
+                                        )
+                                f.write(line + '\n')
