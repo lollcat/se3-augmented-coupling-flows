e9b7e9846830caa4c48be73e98f32c77f257840e
lollcat <laurencemidgley@gmail.com>
2023-05-04 16:03:30 +0100

    feat: lj 13 add harmonic potential

--- examples/lj13_fab.py
+++ examples/lj13_fab.py
@@ -23,8 +23,10 @@ def to_local_config(cfg: DictConfig) -> DictConfig:
     cfg.flow.n_aug = 1
     cfg.fab.eval_inner_batch_size = 2
     cfg.fab.eval_total_batch_size = 4
-    cfg.fab.buffer_min_length = cfg.training.batch_size * cfg.fab.n_updates_per_smc_forward_pass + 1
-    cfg.fab.buffer_max_length = cfg.training.batch_size * cfg.fab.n_updates_per_smc_forward_pass * 10
+    cfg.fab.n_updates_per_smc_forward_pass = 2
+    cfg.fab.n_intermediate_distributions = 1
+    cfg.fab.buffer_min_length_batches = 4
+    cfg.fab.buffer_max_length_batches = 10
 
     cfg.training.n_epoch = 32
     cfg.training.save = False
@@ -41,7 +43,7 @@ def to_local_config(cfg: DictConfig) -> DictConfig:
 
 @hydra.main(config_path="./config", config_name="lj13_fab.yaml")
 def run(cfg: DictConfig):
-    local_config = False
+    local_config = True
     if local_config:
         cfg = to_local_config(cfg)
 
