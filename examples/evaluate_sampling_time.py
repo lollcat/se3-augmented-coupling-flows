caffef0350390eb407a477b14b149633a1194913
lollcat <laurencemidgley@gmail.com>
2023-05-14 09:15:10 +0100

    feat: evaluate sampling time

--- examples/evaluate_sampling_time.py
+++ examples/evaluate_sampling_time.py
@@ -3,17 +3,26 @@ from omegaconf import DictConfig
 import yaml
 import time
 
-from molboil.targets.data import load_dw4
+from molboil.targets.data import load_dw4, load_lj13, load_qm9
 from examples.create_train_config import create_flow_config, AugmentedFlow, TrainingState
 from examples.create_train_config import build_flow
 
 
-def sampling_time_dw4():
+def sampling_time(problem = "dw4"):
     key = jax.random.PRNGKey(0)
-    cfg = DictConfig(yaml.safe_load(open(f"examples/config/dw4.yaml")))
-    train_data, valid_data, test_data = load_dw4(train_set_size=1,
-                                                 test_set_size=1,
-                                                 val_set_size=1)
+    if problem == "dw4":
+        cfg = DictConfig(yaml.safe_load(open(f"examples/config/dw4.yaml")))
+        train_data, _, _ = load_dw4(train_set_size=1,
+                                                     test_set_size=1,
+                                                     val_set_size=1)
+    elif problem == "lj13":
+        cfg = DictConfig(yaml.safe_load(open(f"examples/config/lj13.yaml")))
+        train_data, _, _ = load_lj13(train_set_size=1)
+    else:
+        assert problem == "qm9"
+        cfg = DictConfig(yaml.safe_load(open(f"examples/config/qm9.yaml")))
+        train_data, _, _ = load_qm9(train_set_size=1)
+
     flow_config = create_flow_config(cfg)
     flow = build_flow(flow_config)
 
@@ -32,6 +41,7 @@ def sampling_time_dw4():
 
 
 
-
+#TODO: Add flow type, time properly, format in latex table
 if __name__ == '__main__':
-    print(sampling_time_dw4())
+    for problem in ["dw4"]: # , "lj13", "qm9"]:
+        print(sampling_time())
