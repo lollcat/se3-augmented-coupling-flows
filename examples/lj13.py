2d72b919d8517d125c3f25506fe4184d0627b71d
lollcat <laurencemidgley@gmail.com>
2023-07-26 10:07:26 +0100

    feat: diff random initialisation per flock block

--- examples/lj13.py
+++ examples/lj13.py
@@ -27,7 +27,7 @@ def to_local_config(cfg: DictConfig) -> DictConfig:
     cfg.flow.n_layers = 1
     cfg.flow.nets.egnn.n_blocks = 1
     cfg.training.batch_size = 2
-    cfg.flow.type = 'spherical'  # 'spherical'
+    cfg.flow.type = 'along_vector'  # 'spherical'
     cfg.flow.kwargs.spherical.spline_num_bins = 3
     cfg.flow.n_aug = 1
 
