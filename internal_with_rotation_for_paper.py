325b5bb94df777b2af3af268b9c4c4ee325fe069
lollcat <laurencemidgley@gmail.com>
2023-08-16 17:54:13 +0100

    feat: fix distribution of z

--- internal_with_rotation_for_paper.py
+++ internal_with_rotation_for_paper.py
@@ -90,8 +90,20 @@ def forward(x: chex.Array) -> chex.Array:
 
 
 def sample_and_log_prob_z1_t1_t2(key: chex.PRNGKey) -> Tuple[chex.Array, chex.Array]:
+    """
+    Represent uniform distribution on sphere by [z, t1].
+    We sample z \propto circumpherence(z) \propto sqrt(1 - z^2)
+    \int_{-1}_{1} sqrt(1 - z^2) dz = pi / 2
+    This gives p(z) = sqrt(1 - z^2) /
+    And also sample uniformly on [-pi, pi] for the atom 2."""
     key1, key2, key3 = jax.random.split(key, 3)
-    z1, log_p_z1 = distrax.Uniform(low=-1., high=1.).sample_and_log_prob(seed=key1)
+
+    xyz = jax.random.normal(key1, shape=(3,))
+    z1 = xyz[-1] / jnp.linalg.norm(xyz)
+
+    p_z1 = jnp.sqrt(1 - z1**2) / (jnp.pi / 2)
+    log_p_z1 = jnp.log(p_z1)
+
     t1, log_p_t1 = distrax.Uniform(low=-jnp.pi, high=jnp.pi).sample_and_log_prob(seed=key2)
     t2, log_p_t2 = distrax.Uniform(low=-jnp.pi, high=jnp.pi).sample_and_log_prob(seed=key2)
     return jnp.stack([z1, t1, t2]), log_p_z1 + log_p_t1 + log_p_t2
