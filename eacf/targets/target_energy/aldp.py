46d180c3429b8281313c76aaac3f787a65fe3625
Vincent Stimper <vincent.stimper@gmail.com>
2023-08-19 22:32:54 +0200

    Added Multiprocessing functionality

--- eacf/targets/target_energy/aldp.py
+++ eacf/targets/target_energy/aldp.py
@@ -1,15 +1,78 @@
 import chex
-import jax
 import jax.numpy as jnp
 import numpy as np
 
 import openmm
 from openmmtools import testsystems
+import multiprocessing as mp
 
 
 R = 8.31447e-3
 
-def openmm_energy(x: chex.Array, openmm_context, temperature: float = 800.):
+def openmm_energy(x: np.ndarray, openmm_context, temperature: float = 800.):
+    """Compute the energy of a single configuration using OpenMM.
+
+    Args:
+        x: A configuration of shape (n_atoms, 3).
+        openmm_context: An OpenMM context.
+        temperature: The temperature in Kelvin.
+
+    Returns:
+        The energy of the configuration.
+    """
+    kBT = R * temperature
+    # Handle nans and infinities
+    if np.any(np.isnan(x)) or np.any(np.isinf(x)):
+        return np.nan
+    else:
+        openmm_context.setPositions(x)
+        state = openmm_context.getState(getForces=False, getEnergy=True)
+
+        # get energy
+        energy = state.getPotentialEnergy().value_in_unit(
+            openmm.unit.kilojoule / openmm.unit.mole) / kBT
+        return energy
+
+
+def openmm_energy_multi_proc(x: np.ndarray):
+    """Compute the energy of a single configuration using OpenMM using global
+    temperature and context.
+
+    Args:
+        x: A configuration of shape (n_atoms, 3).
+
+    Returns:
+        The energy of the configuration.
+    """
+    kBT = R * temperature
+    # Handle nans and infinities
+    if np.any(np.isnan(x)) or np.any(np.isinf(x)):
+        return np.nan
+    else:
+        openmm_context.setPositions(x)
+        state = openmm_context.getState(getForces=False, getEnergy=True)
+
+        # get energy
+        energy = state.getPotentialEnergy().value_in_unit(
+            openmm.unit.kilojoule / openmm.unit.mole) / kBT
+        return energy
+
+def openmm_multi_proc_init(sys, temp):
+    """
+    Method to initialize temperature and openmm context for workers
+    of multiprocessing pool
+    """
+    global temperature, openmm_context
+    temperature = temp
+    sim = openmm.app.Simulation(sys.topology, sys.system,
+                                openmm.LangevinIntegrator(temp * openmm.unit.kelvin,
+                                                          1.0 / openmm.unit.picosecond,
+                                                          1.0 * openmm.unit.femtosecond),
+                                platform=openmm.Platform.getPlatformByName('Reference'))
+    openmm_context = sim.context
+
+
+def openmm_energy_batched(x: chex.Array, openmm_context, temperature: float = 800.):
     """Compute the energy of a batch of configurations using OpenMM.
 
     Args:
@@ -22,24 +85,19 @@ def openmm_energy(x: chex.Array, openmm_context, temperature: float = 800.):
     """
     n_batch = x.shape[0]
 
-    kBT = R * temperature
     energies = []
     for i in range(n_batch):
         # Get numpy array as input for OpenMM
         x_np = np.array(x[i, ...])
-        # Handle nans and infinities
-        if np.any(np.isnan(x_np)) or np.any(np.isinf(x_np)):
-            energies.append(np.nan)
-        else:
-            openmm_context.setPositions(x_np)
-            state = openmm_context.getState(getForces=False, getEnergy=True)
-
-            # get energy
-            energies.append(
-                state.getPotentialEnergy().value_in_unit(
-                    openmm.unit.kilojoule / openmm.unit.mole) / kBT
-            )
+        energies.append(openmm_energy(x_np, openmm_context, temperature))
     return jnp.array(energies)
+
+
+def openmm_energy_multi_proc_batched(x: chex.Array, pool):
+    x_np = np.array(x)
+    energies_out = pool.map(openmm_energy_multi_proc, x_np)
+    energies = jnp.array(energies_out)
+    return energies
 
 
 def get_log_prob_fn(temperature: float = 800, environment: str = 'implicit'):
@@ -68,10 +126,47 @@ def get_log_prob_fn(temperature: float = 800, environment: str = 'implicit'):
 
     def log_prob_fn(x: chex.Array):
         if len(x.shape) == 2:
-            energy = openmm_energy(x[None, ...], sim.context, temperature=temperature)
+            energy = openmm_energy_batched(x[None, ...], sim.context, temperature=temperature)
             return -energy[0, ...]
         elif len(x.shape) == 3:
-            energies = openmm_energy(x, sim.context, temperature=temperature)
+            energies = openmm_energy_batched(x, sim.context, temperature=temperature)
+            return -energies
+        else:
+            raise NotImplementedError('The OpenMM energy function only supports 2D and 3D inputs')
+
+    return log_prob_fn
+
+
+def get_multi_proc_log_prob_fn(temperature: float = 800, environment: str = 'implicit', n_threads: int = 18):
+    """Get a function that computes the energy of a batch of configurations via multiprocessing.
+
+    Args:
+        temperature (float, optional): The temperature in Kelvin. Defaults to 800.
+        environment (str, optional): The environment in which the energy is computed. Can be implicit or vacuum.
+        Defaults to 'implicit'.
+        n_threads (int, optional): Number of threads for multiprocessing. Defaults to 18.
+
+    Returns:
+        A function that computes the energy of a batch of configurations.
+    """
+    # System setup
+    if environment == 'vacuum':
+        system = testsystems.AlanineDipeptideVacuum(constraints=None)
+    elif environment == 'implicit':
+        system = testsystems.AlanineDipeptideImplicit(constraints=None)
+    else:
+        raise NotImplementedError('This environment is not implemented.')
+
+    # Initialize multiprocessing pool
+    pool = mp.Pool(n_threads, openmm_multi_proc_init, (system, temperature))
+
+    # Define function
+    def log_prob_fn(x: chex.Array):
+        if len(x.shape) == 2:
+            energy = openmm_energy_multi_proc_batched(x[None, ...], pool)
+            return -energy[0, ...]
+        elif len(x.shape) == 3:
+            energies = openmm_energy_multi_proc_batched(x, pool)
             return -energies
         else:
             raise NotImplementedError('The OpenMM energy function only supports 2D and 3D inputs')
--- eacf/targets/target_energy/aldp_test.py
+++ eacf/targets/target_energy/aldp_test.py
@@ -1,9 +1,8 @@
 from pathlib import Path
-import jax
 import jax.numpy as jnp
 
 from eacf.targets.data import load_aldp
-from eacf.targets.target_energy.aldp import get_log_prob_fn
+from eacf.targets.target_energy.aldp import get_log_prob_fn, get_multi_proc_log_prob_fn
 
 
 def test_aldp_log_prob():
@@ -21,5 +20,28 @@ def test_aldp_log_prob():
     assert jnp.shape(log_prob) == (100,)
     assert jnp.all(jnp.isfinite(log_prob))
 
+
+def test_aldp_multi_proc_log_prob():
+    # Get log prob function
+    log_prob_mp_fn = get_multi_proc_log_prob_fn(temperature=500., n_threads=8)
+    log_prob_fn = get_log_prob_fn(temperature=500.)
+
+    # Load sample data
+    path = Path(__file__).parent.parent / "data" / "aldp_500K_train_mini.h5"
+    aldp_data = load_aldp(train_path=path, train_n_points=100)[0]
+
+    # Compute log prob
+    log_prob_mp = log_prob_mp_fn(aldp_data.positions)
+    log_prob = log_prob_fn(aldp_data.positions)
+
+    # Check log prob
+    assert jnp.shape(log_prob_mp) == (100,)
+    assert jnp.shape(log_prob) == (100,)
+    assert jnp.all(jnp.isfinite(log_prob_mp))
+    assert jnp.all(jnp.isfinite(log_prob))
+    assert jnp.allclose(log_prob_mp, log_prob)
+
+
 if __name__ == "__main__":
     test_aldp_log_prob()
+    test_aldp_multi_proc_log_prob()
